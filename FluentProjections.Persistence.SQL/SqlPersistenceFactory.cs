794d988e27ee10bb49a59572316b84b0103d1450
Michael Borisov <corker@inbox.ru>
2016-01-20 18:53:18 +0100

    implementation and tests finished

---
+++ FluentProjections.Persistence.SQL/SqlPersistenceFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+
+namespace FluentProjections.Persistence.SQL
+{
+    public class SqlPersistenceFactory : ICreateProjectionProviders
+    {
+        private readonly Func<IDbConnection> _connections;
+
+        public SqlPersistenceFactory(Func<IDbConnection> connections)
+        {
+            _connections = connections;
+        }
+
+        public IProvideProjections Create()
+        {
+            var connection = _connections();
+            return new SqlPersistence(connection);
+        }
+    }
+}
